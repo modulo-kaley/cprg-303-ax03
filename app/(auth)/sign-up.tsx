988e298e3696d14f4512e2105ce13a9eb1360e37
Joshua Couto <joshua.s.couto@gmail.com>
2026-03-08 15:32:57 -0600

    Added File Headers

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,1 +1,8 @@
+/**
+ * @author Group 9 - Aaron Reid, Joshua Couto, Kaley Wood
+ * Southern Alberta Institute of Technology: CPRG-303-C
+ * Assignment 3: Advanced Form Development and Validation with React Hook Form & Zod
+ * Created: 03.08.2026
+ */
+
 // router/push('/auth)/sign-up')
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,12 @@
 /**
  * @author Group 9 - Aaron Reid, Joshua Couto, Kaley Wood
  * Southern Alberta Institute of Technology: CPRG-303-C
- * Assignment 2: Advanced Multi-Screen Mobile App w/ Collaborative Navigation
- * Created: 03.01.2026
+ * Assignment 3: Advanced Form Development and Validation with React Hook Form & Zod
+ * Created: 03.08.2026
  */
 
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function Index(){
+    return <Redirect href="/onboarding" />;
 }
